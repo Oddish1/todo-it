6f4df457ddeba767bd5a3f402102ce4eb5a0d2a1
[Austin Winslow] <128003679+Oddish1@users.noreply.github.com>
2024-03-13 17:38:06 -0700

    added auth blueprints renamed app to work with imports

---
+++ todo_it/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+import os
+from . import db, auth
+
+
+def create_app(test_config=None):
+  # create and configure the app
+  app = Flask(__name__)
+  app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+  )
+
+  if test_config is None:
+    # load the instance config
+    app.config.from_pyfile('config.py', silent=True)
+  else:
+    # load the test config
+    app.config.from_mapping(test_config)
+
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+
+  db.init_app(app)
+  
+  app.register_blueprint(auth.bp)
+
+  @app.route('/')
+  def index():
+    return 'Hello, World!'
+
+  return app
