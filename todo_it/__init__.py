f27ee529a7d0829ea5c41e61ebefad2b2340214b
[Austin Winslow] <128003679+Oddish1@users.noreply.github.com>
2024-03-14 17:29:34 -0700

    Implemented CRUD for tasks.

--- todo_it/__init__.py
+++ todo_it/__init__.py
@@ -1,35 +1,34 @@
 from flask import Flask
 from flask import render_template
 import os
-from . import db, auth
+from . import db, auth, tasks
 
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-  )
+    # create and configure the app
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "app.sqlite"),
+    )
 
-  if test_config is None:
-    # load the instance config
-    app.config.from_pyfile('config.py', silent=True)
-  else:
-    # load the test config
-    app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config
+        app.config.from_mapping(test_config)
 
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-  db.init_app(app)
-  
-  app.register_blueprint(auth.bp)
+    db.init_app(app)
 
-  @app.route('/')
-  def index():
-    return 'Hello, World!'
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(tasks.bp)
 
-  return app
+    app.add_url_rule('/', endpoint='index')
+
+    return app
---
+++ todo_it/tasks.py
@@ -0,0 +1,98 @@
+from flask import (
+    Blueprint,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    url_for
+)
+from werkzeug.exceptions import abort
+
+from todo_it.auth import login_required
+from todo_it.db import get_db
+
+bp = Blueprint('tasks', __name__)
+
+@bp.route('/')
+def index():
+    db = get_db()
+    tasks = db.execute(
+        'SELECT t.task_id, owner_id, created, task_name, task_description, task_complete'
+        ' FROM tasks t JOIN users u ON t.owner_id = u.user_id'
+        #' ORDER BY created DESC'
+    ).fetchall()
+    return render_template('tasks/index.html', tasks=tasks)
+
+
+@bp.route('/new', methods=('GET', 'POST'))
+@login_required
+def new():
+    if request.method == 'POST':
+        task_name = request.form['task_name']
+        task_description = request.form['task_description']
+        error = None
+
+        if not task_name:
+            error = 'Your task needs a name!'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO tasks (task_name, task_description, owner_id)'
+                ' VALUES (?, ?, ?)',
+                (task_name, task_description, g.user['user_id'])
+            )
+            db.commit()
+            return redirect(url_for('tasks.index'))
+    return render_template('tasks/new.html')
+
+
+def get_task(task_id, check_owner=True):
+    task = get_db().execute(
+        'SELECT t.task_id, owner_id, created, due_date, task_name, task_description, task_priority, task_complete'
+        ' FROM tasks t JOIN users u ON t.owner_id=u.user_id'
+        ' WHERE t.task_id = ?',
+        (task_id,)
+    ).fetchone()
+
+    if task is None:
+        abort(404, f'Task id {task_id} doesn\'t exist.')
+    if check_owner and task['owner_id'] != g.user['user_id']:
+        abort(403)
+    
+    return task
+
+@bp.route('/<int:task_id>/update', methods=('GET', 'POST'))
+@login_required
+def update(task_id):
+    task = get_task(task_id)
+
+    if request.method == 'POST':
+        task_name = request.form['task_name']
+        task_description = request.form['task_description']
+        error = None
+        if not task_name:
+            error = 'Your task needs a name!'
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE tasks SET task_name = ?, task_description = ?'
+                ' WHERE task_id = ?',
+                (task_name, task_description, task_id)
+            )
+            db.commit()
+            return redirect(url_for('tasks.index'))
+    return render_template('tasks/update.html', task=task)
+
+@bp.route('/<int:task_id>/delete', methods=('POST',))
+@login_required
+def delete(task_id):
+    get_task(task_id)
+    db = get_db()
+    db.execute('DELETE FROM tasks WHERE task_id = ?', (task_id,))
+    db.commit()
+    return redirect(url_for('tasks.index'))
